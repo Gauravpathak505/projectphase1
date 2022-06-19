ac1ef94907f91dc89af44f6ec5cb561f7675cd87
gauravpathak5045 <gauravpathak505@gmail.com>
2022-06-19 18:05:13 +0530

    first commit

---
+++ src/SuperBond.java
@@ -0,0 +1,12 @@
+public class SuperBond {
+
+    public static void main(String[] args) {
+
+        filroPeration.createMainFolderIfNotPresent("main");
+
+        Menu.printWelcomeScreen("Super Bond Lockers Pvt. Ltd", "Gaurav pathak ");
+
+        Handle.handleWelcomeScreenInput();
+    }
+
+}
